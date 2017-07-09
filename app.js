fefe3c66b6a443a089a75addc7fdec8e4cab27dd
eliserothberg <rothberg.elise@gmail.com>
2017-07-09 01:17:33 -0700

    added date and time parsing, exploring form input

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 // var moment = require('moment');
 
 // set home and work addresses as global variables 
-//(they are doubled right now but tone set will go away)
+//this will not be hardcoded
 var homeAddress = '3645 Westwood Blvd, Los Angeles, CA 90034';
 var workAddress = '200 N Spring St, Los Angeles, CA 90012';
-// var homeStreetAddress = '3670 Westwood Boulevard, Los Angeles, CA';
-// var workStreetAddress = '1117 S Figueroa St, Los Angeles, CA';
 
-//Leave at 8:00AM 
-var timeToWork = new Date(1499698800000);
-
-//Leave at 5:30PM
-var timeToHome = new Date(1499733000000);
 //setting global variables
+var timeToWork;
+var timeToHome;
 var distanceAndTime;
 var theTime;
 var monetaryCost;
@@ -22,8 +17,14 @@ var annualTransitTime = [];
 var latLongAddresses = [];
 var timeTravelArray = [];
 var transitTravelArray = [];
+var times = [];
 var homeLLAddress;
 var workLLAddress;
+// var toWorkAM;
+// var toHomeAM;
+var inAM;
+var travelingAt = [];
+// var userInput = [];
 
 //function to use google maps API
 function initMap() {
@@ -46,8 +47,8 @@ function initMap() {
         homeLLAddress = "{lat:" + latLongAddresses[0] + ",lng:" + latLongAddresses[1] + "}";
         workLLAddress = "{lat:" + latLongAddresses[2] + ",lng:" + latLongAddresses[3] + "}";
       
-        console.log("lat/lon home: " + homeLLAddress);
-        console.log("lat/lon work: " + workLLAddress);
+        // console.log("lat/lon home: " + homeLLAddress + "time to work: " + timeToWork);
+        // console.log("lat/lon work: " + workLLAddress + "time home: " + timeToHome);
       }
     });
   }
@@ -55,7 +56,61 @@ function initMap() {
   LLaddress(homeAddress);
   LLaddress(workAddress);
 
-  //use Google Maps API to calculate distances and travel times
+  //Set today as the date object
+  var today = new Date();
+
+  //change the date to next Wednesday
+  function setTimes(hours, minutes, amOrPm) {
+  var nextWednesday = new Date();
+  nextWednesday.setDate(nextWednesday.getDate() + (9-nextWednesday.getDay())%7+1);
+  //Apply users departure time data
+  nextWednesday.setHours(hours, minutes);
+  travelingAt.push(nextWednesday);
+
+    if (travelingAt.length == 2) {
+
+      timeToWork = travelingAt[0];
+      timeToHome = travelingAt[1];
+
+        //change the date to the following Wednesday and the one after that to add then average- ??
+      // timeToWork2 = new Date(nextWednesday.setDate(nextWednesday.getDate() + (16-nextWednesday.getDay())%7+1));
+      // timeToWork3 = new Date(nextWednesday.setDate(nextWednesday.getDate() + (23-nextWednesday.getDay())%7+1));
+    }
+
+  //set up times to display as XX:XX AM or XX:XX PM
+    Date.prototype.timesOnly = function() {
+      var hours = this.getHours() %12 || 12;
+      hours = hours.toString();
+      var minutes = this.getMinutes().toString();
+      return (hours[1]?hours:"0"+hours[0]%12) + ':' + (minutes[1]?minutes:"0"+minutes[0]);
+    };
+      
+    var timeOfDay = nextWednesday.timesOnly()
+    //determine AM or PM
+    if (amOrPm == true) {
+      timeOfDay = timeOfDay + ' AM';
+    }
+    else {
+      timeOfDay = timeOfDay + ' PM';
+    }
+
+    times.push(timeOfDay);
+
+    if (times.length == 2) {
+      console.log("Home: " + homeAddress + "    Work: " + workAddress);
+      console.log("Leaving for work at: " + times[0]);
+      console.log("Leaving for home at: " + times[1]);
+    }
+  }
+
+  //this will not be hardcoded
+  var hrs = 8;
+  var mnts = 0;
+  var am = true;
+  setTimes(hrs, mnts, am);
+  setTimes(17, 30, false);
+
+  // use Google Maps API to calculate distances and travel times
   function modes(theMode, time) {
     //straight from the docs
     var service = new google.maps.DistanceMatrixService;
@@ -64,11 +119,12 @@ function initMap() {
       destinations: [workAddress],
       travelMode: theMode,
       transitOptions: {
+        modes: ['RAIL'],
         departureTime: time
       },
       drivingOptions: {
-        departureTime: time,
-        trafficModel: 'pessimistic'
+        trafficModel: 'pessimistic',
+        departureTime: time
       },
       unitSystem: google.maps.UnitSystem.IMPERIAL,
       avoidHighways: false,
@@ -90,74 +146,74 @@ function initMap() {
           
           for (var j = 0; j < results.length; j++) {
             var theAddresses = originList[i].slice(0, -15) + ' to ' + destinationList[j].slice(0, -15);
-              if (theMode === "DRIVING") {
-                var distanceResult = results[j].distance.text;
-                //slice " mi" off the distance return value
-                var trimDistance = distanceResult.slice(0, -3);
-                var theDistance = results[j].duration_in_traffic.text;
-                //adjust travel time in traffic from seconds to minutes
-                var timeEachWay = (results[j].duration_in_traffic.value / 60).toFixed(0);
-                //push travel times each way into array
-                timeTravelArray.push(timeEachWay);
-                // calculate annual costs of driving using year average of 251 work days 
-                // and $0.26 cents of wear, tear, and gas
-                var cost = (Number(trimDistance) * .26) * 502;
-                var lengthDrive = results[j].duration_in_traffic.value
-                //calculate annual drive time for each way of commute
-                var timeDrive = ((Math.round(lengthDrive*Math.pow(10,2))/Math.pow(10,2)) / 60).toFixed(0);
-                var annualHalfDriveTime = Number(timeDrive) * 251;
-                //push commute times to a variable
-                annualDriveTime.push(annualHalfDriveTime);
-                //Once both times are in the array, add commute times for total driving time
-                if (annualDriveTime.length == 2) {
-                  totalAnnualCommuteTime = annualDriveTime[0] + annualDriveTime[1];  
-                  //use convertTime function, store in variables
-                  var totalTravelTime = convertTime(totalAnnualCommuteTime);
-                  var commuteToWork = convertTime(annualDriveTime[0]);
-                  var commuteHome = convertTime(annualDriveTime[1]);
-                  var timeToWork = convertTime(timeTravelArray[0]);
-                  var timeToHome = convertTime(timeTravelArray[1]);
-                } 
-                //Put costs in decimal format
-                monetaryCost = Math.round(cost*Math.pow(10,2))/Math.pow(10,2).toFixed(2)
-                //Put distance and times into one variable
-                var distanceAndTime = results[j].distance.text + ' each way. Home to work in ' +  timeToWork + "; work to home in " + timeToHome;                
-              }  
+            if (theMode === "DRIVING") {
+              var distanceResult = results[j].distance.text;
+              //slice " mi" off the distance return value
+              var trimDistance = distanceResult.slice(0, -3);
+              var theDistance = results[j].duration_in_traffic.text;
+              //adjust travel time in traffic from seconds to minutes
+              var timeEachWay = (results[j].duration_in_traffic.value / 60).toFixed(0);
+              //push travel times each way into array
+              timeTravelArray.push(timeEachWay);
+              // calculate annual costs of driving using year average of 251 work days 
+              // and $0.26 cents of wear, tear, and gas
+              var cost = (Number(trimDistance) * .26) * 502;
+              var lengthDrive = results[j].duration_in_traffic.value
+              //calculate annual drive time for each way of commute
+              var timeDrive = ((Math.round(lengthDrive*Math.pow(10,2))/Math.pow(10,2)) / 60).toFixed(0);
+              var annualHalfDriveTime = Number(timeDrive) * 251;
+              //push commute times to a variable
+              annualDriveTime.push(annualHalfDriveTime);
+              //Once both times are in the array, add commute times for total driving time
+              if (annualDriveTime.length == 2) {
+                totalAnnualCommuteTime = annualDriveTime[0] + annualDriveTime[1];  
+                //use convertTime function, store in variables
+                var totalTravelTime = convertTime(totalAnnualCommuteTime);
+                var commuteToWork = convertTime(annualDriveTime[0]);
+                var commuteHome = convertTime(annualDriveTime[1]);
+                var timeToWork = convertTime(timeTravelArray[0]);
+                var timeToHome = convertTime(timeTravelArray[1]);
+              } 
+              //Put costs in decimal format
+              monetaryCost = Math.round(cost*Math.pow(10,2))/Math.pow(10,2).toFixed(2)
+              //Put distance and times into one variable
+              var distanceAndTime = results[j].distance.text + ' each way. Home to work in ' +  timeToWork + "; work to home in " + timeToHome;                
+            }  
 
-              else if (theMode === "TRANSIT") {
-                var theDistance = results[j].duration.text
-                //adjust travel time in traffic from seconds to minutes
-                var oneWayTravel = (results[j].duration.value / 60).toFixed(0);
-                //store adjusted 
-                transitTravelArray.push(oneWayTravel);
-                // calculate annual cost of taking transit using year average of 251 work days 
-                monetaryCost = (1.75 * 502).toFixed(2);
-                //calculate annual travel time for each way of commute
-                var transitTimeOneWay = (Math.round(oneWayTravel*Math.pow(10,2))/Math.pow(10,2))
-                var annualTransitOneWay = Number(transitTimeOneWay) * 251;
-                var transitTimeOneWay = ((Number(results[j].duration.value) * 251) / 60).toFixed(0);
-                //push commute times to a variable
-                annualTransitTime.push(annualTransitOneWay);
-                //Once both times are in the array, add commute times for total transit time
-                if (annualTransitTime.length == 2) {
-                  totalAnnualCommuteTime = annualTransitTime[0] + annualTransitTime[1];  
-                  //use convertTime function, store in variables
-                  var totalTravelTime = convertTime(totalAnnualCommuteTime);
-                  var commuteToWork = convertTime(annualTransitTime[0]);
-                  var commuteHome = convertTime(annualTransitTime[1]);
-                  var timeToWork = convertTime(transitTravelArray[0]);
-                  var timeToHome = convertTime(transitTravelArray[1]);
-                }
-              var distanceAndTime = results[j].distance.text + ' each way. Home to work in ' +  timeToWork + "; work to home in " + timeToHome;
+            else if (theMode === "TRANSIT") {
+              var theDistance = results[j].duration.text
+              //adjust travel time in traffic from seconds to minutes
+              var oneWayTravel = (results[j].duration.value / 60).toFixed(0);
+              //store adjusted 
+              transitTravelArray.push(oneWayTravel);
+              // calculate annual cost of taking transit using year average of 251 work days 
+              monetaryCost = (1.75 * 502).toFixed(2);
+              //calculate annual travel time for each way of commute
+              var transitTimeOneWay = (Math.round(oneWayTravel*Math.pow(10,2))/Math.pow(10,2))
+              var annualTransitOneWay = Number(transitTimeOneWay) * 251;
+              var transitTimeOneWay = ((Number(results[j].duration.value) * 251) / 60).toFixed(0);
+              //push commute times to a variable
+              annualTransitTime.push(annualTransitOneWay);
+              //Once both times are in the array, add commute times for total transit time
+              if (annualTransitTime.length == 2) {
+                totalAnnualCommuteTime = annualTransitTime[0] + annualTransitTime[1];  
+                //use convertTime function, store in variables
+                var totalTravelTime = convertTime(totalAnnualCommuteTime);
+                var commuteToWork = convertTime(annualTransitTime[0]);
+                var commuteHome = convertTime(annualTransitTime[1]);
+                var timeToWork = convertTime(transitTravelArray[0]);
+                var timeToHome = convertTime(transitTravelArray[1]);
               }
+            var distanceAndTime = results[j].distance.text + ' each way. Home to work in ' +  timeToWork + "; work to home in " + timeToHome;
             }
           }
         }
-        if (annualTransitTime.length == 2 || annualDriveTime.length == 2) {
-          // console.log(theAddresses + ":")
-          console.log(theMode + ": " + distanceAndTime)
-          console.log(theMode + ": Yearly cost: $" + monetaryCost + ", Total commute: " + totalTravelTime);
-        }
+      }
+      if (annualTransitTime.length == 2 || annualDriveTime.length == 2 && totalTravelTime != 'undefined') {
+        // console.log(theAddresses + ":")
+        console.log(theMode + ": " + distanceAndTime)
+        console.log("         Yearly cost: $" + monetaryCost + ", Total commute: " + totalTravelTime);
+      }
     });
   }
   modes('DRIVING', timeToWork)
